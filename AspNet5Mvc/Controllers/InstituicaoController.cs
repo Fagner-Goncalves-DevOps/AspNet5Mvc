49af5453f04defa3679ddd5133f989093c75bb59
Fagner <fagnergoncalves15@gmail.com>
2022-07-27 01:42:52 -0300

    MVC Fundantos necessarios

--- AspNet5Mvc/Controllers/InstituicaoController.cs
+++ AspNet5Mvc/Controllers/InstituicaoController.cs
@@ -21,17 +21,19 @@ namespace AspNet5Mvc.Controllers
         };
 
         /*
-        action result ja eh uma classe abstrata, outras classes são estendidas
-        quanto mais descer  o nível hierárquico das interfaces e classes, mais especializados ficam os tipos de retorno para as actions.
+        ActionResult ja é uma classe abstrata, outras classes são estendidas
+        quanto mais descer o nível hierárquico das interfaces e classes, mais especializados ficam os tipos de retorno para as actions.
         existe tambem outras formas de retornos especializados 
         */
-        
+
         public IActionResult Index() 
         {
-            return View(_instituicaos.OrderBy(i =>i.Nome));
+            return View(_instituicaos);
+            //_instituicaos.OrderBy(i =>i.Nome) para ordenação
         }
- 
 
+
+        //Criar novo
         public IActionResult Create()
         {
             return View();
@@ -46,6 +48,8 @@ namespace AspNet5Mvc.Controllers
             return RedirectToAction("Index");
         }
 
+
+        //Editar
         public ActionResult Edit(long id)
         {
             return View(_instituicaos.Where(i => i.InstituicaoID == id).FirstOrDefault());
@@ -56,20 +60,16 @@ namespace AspNet5Mvc.Controllers
         {
             _instituicaos.Remove(_instituicaos.Where(i => i.InstituicaoID == instituicao.InstituicaoID).FirstOrDefault());
             _instituicaos.Add(instituicao);
-            //_instituicaos[_instituicaos.IndexOf(_instituicaos.Where(i=> i.InstituicaoID == instituicao.InstituicaoID).FirstOrDefault())] = instituicao;
-            //_instituicaos.Add(instituicao);
             return RedirectToAction("Index");
         }
 
+        //Detalhes
         public ActionResult Details(long id) 
         {
-            var detalhes = _instituicaos.Where(i => i.InstituicaoID == id).FirstOrDefault();
-            return View();
+            return View(_instituicaos.Where(i => i.InstituicaoID == id).FirstOrDefault());
         }
 
-
-
-        //deletando
+        //Deletar
         public ActionResult Delete(long id) 
         {
             return View(_instituicaos.Where(i => i.InstituicaoID == id).FirstOrDefault()) ;
@@ -83,6 +83,7 @@ namespace AspNet5Mvc.Controllers
         }
 
 
-
+        //_instituicaos[_instituicaos.IndexOf(_instituicaos.Where(i=> i.InstituicaoID == instituicao.InstituicaoID).FirstOrDefault())] = instituicao;
+        //_instituicaos.Add(instituicao);
     }
 }
