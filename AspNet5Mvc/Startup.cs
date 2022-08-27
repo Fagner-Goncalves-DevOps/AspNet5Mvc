19e6831f4b1c80bdaf85a33b33ad28470f84798d
Fagner <fagnergoncalves15@gmail.com>
2022-08-27 01:49:25 -0300

    MVC Fundantos necessarios

--- AspNet5Mvc/Startup.cs
+++ AspNet5Mvc/Startup.cs
@@ -23,7 +23,6 @@ namespace AspNet5Mvc
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -44,7 +43,6 @@ namespace AspNet5Mvc
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
