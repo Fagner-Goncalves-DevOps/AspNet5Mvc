c31382a30d983d66645fb8317588f5b5e0b523d8
Fagner <fagnergoncalves15@gmail.com>
2022-08-27 00:28:50 -0300

    MVC Fundantos necessarios

---
+++ AspNet5Mvc/Areas/Docente/Models/AdicionarProfessorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNet5Mvc.Areas.Docente.Models
+{
+    public class AdicionarProfessorViewModel
+    {
+        public long? InstituicaoID { get; set; }
+        public long? DepartamentoID { get; set; }
+        public long? CursoID { get; set; }
+        public long? ProfessorID { get; set; }
+    }
+}
